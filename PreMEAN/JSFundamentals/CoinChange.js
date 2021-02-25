bee75d830f871db21b630b8908c974a9272bf99b
Cthulhu <sramirez_nicholas@yahoo.com>
2021-02-25 08:12:42 -0800

    premean

--- PreMEAN/JSFundamentals/CoinChange.js
+++ PreMEAN/JSFundamentals/CoinChange.js
@@ -1,5 +1,11 @@
 function coinChange(n, obj) {
     var change = {
+        hundredDollar: 0,
+        fiftyDollar: 0,
+        twentyDollar: 0,
+        tenDollar: 0,
+        fiveDollar: 0,
+        twoDollar: 0,
         dollar: 0,
         halfDollar: 0,
         quarter: 0,
@@ -8,51 +14,58 @@ function coinChange(n, obj) {
         penny: 0,
     }
 
-    for (var i = 0; i < n; i++) {
-        while (n > 0) {
-            if (n - 100 >= 0) {
-                dollar += 1;
-                n = n - 100;
-                // console.log(dollar);
-            }
+    for (var i = n; i > 0;) {
+        if (i - 10000 >= 0) {
+            change.hundredDollar += 1;
+            i = i - 10000;
         }
-        while (n > 0) {
-            if (n - 50 >= 0) {
-                halfDollar += 1;
-                n = n - 50;
-                // console.log(halfDollar);
-            }
+        if (i - 5000 >= 0) {
+            change.fiftyDollar += 1;
+            i = i - 5000;
         }
-        while (n > 0) {
-            if (n - 25 >= 0) {
-                quarter += 1;
-                n = n - 25;
-                // console.log(quarter);
-            }
+        if (i - 2000 >= 0) {
+            change.twentyDollar += 1;
+            i = i - 2000;
         }
-        while (n > 0) {
-            if (n - 10 >= 0) {
-                dime += 1;
-                n = n - 10;
-                // console.log(dime);
-            }
+        if (i - 1000 >= 0) {
+            change.tenDollar += 1;
+            i = i - 1000;
         }
-        while (n > 0) {
-            if (n - 5 >= 0) {
-                nickel += 1;
-                n = n - 5;
-                // console.log(nickel);
-            }
+        if (i - 500 >= 0) {
+            change.fiveDollar += 1;
+            i = i - 500;
         }
-        while (n > 0) {
-            if (n - 1 >= 0) {
-                penny += 1;
-                n = n - 1;
-                // console.log(penny);
-            }
+        if (i - 200 >= 0) {
+            change.twoDollar += 1;
+            i = i - 200;
+        }
+        if (i - 100 >= 0) {
+            change.dollar += 1;
+            i = i - 100;
+        }
+        else if (i - 50 >= 0) {
+            change.halfDollar += 1;
+            i = i - 50;
+        }
+        else if (i - 25 >= 0) {
+            change.quarter += 1;
+            i = i - 25;
+        }
+        else if (i - 10 >= 0) {
+            change.dime += 1;
+            i = i - 10;
+        }
+        else if (i - 5 >= 0) {
+            change.nickel += 1;
+            i = i - 5;
+        }
+        else if (i - 1 >= 0) {
+            change.penny += 1;
+            i = i - 1;
         }
     }
 
+    return Object.entries(change).forEach(([key, value]) => filter(val => val.value > 0));
 
-    return (change);
+    // return Object.entries(change).filter(val => val.value > 0);
 }
