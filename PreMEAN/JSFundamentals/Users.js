ec6c31efc9da59e7524281dbe3483ea87c4b172e
Cthulhu <sramirez_nicholas@yahoo.com>
2021-02-25 12:03:30 -0800

    PreMean

--- PreMEAN/JSFundamentals/Users.js
+++ PreMEAN/JSFundamentals/Users.js
@@ -1,3 +1,16 @@
 function userLanguages(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var fname = arr[i].fname;
+        var lname = arr[i].lname;
+        var langsArr = [];
+        for (var j = 0; j < arr[i].languages.length; j++) {
+            langsArr.push(arr[i].languages[j]);
+        }
+        // var langs = ""
+        // for (var k = 0; k < langsArr.length; k++) {
+        //     langs.concat(langsArr[k]);
+        // }
+        console.log(fname, lname, "knows", langsArr)
 
+    }
 }
