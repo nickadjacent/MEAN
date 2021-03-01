64b5eeaea58d6148c6a9901c68f6063674f7e71a
Cthulhu <sramirez_nicholas@yahoo.com>
2021-03-01 10:30:15 -0800

    update

--- PreMEAN/JSFundamentals/BinarySearch.js
+++ PreMEAN/JSFundamentals/BinarySearch.js
@@ -2,6 +2,17 @@ function binarySearch(arr, n) {
     var low = 0,
         high = arr.length - 1;
     while (low <= high) {
-
+        const mid = Math.floor((low + high) / 2);
+        const guess = arr[mid];
+        if (guess === n) {
+            return mid
+        }
+        if (guess > n) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
     }
+    return -1;
 }
